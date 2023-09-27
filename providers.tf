f135cdb25a94473326e9833277f4e8afc0e19b2c
ElDelak <mabrouk.kha@gmail.com>
2023-09-27 17:30:35 -0400

    #21 bucket should no longer use random provider (#22)

--- providers.tf
+++ providers.tf
@@ -7,10 +7,10 @@ terraform {
     }
   } */
   required_providers {
-    random = {
+    /* random = {
       source = "hashicorp/random"
       version = "3.5.1"
-    }
+    } */
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
