1b6583227fb5549418c50d573d45715b99cf0cc8
Khaled Mabrouk <mabrouk.kha@gmail.com>
2023-09-27 22:03:25 +0000

    #23 refactor s3 bucket into the new terrahouse_module

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-/* resource "random_string" "bucket_name" {
-  length           = 32
-  special          = false
-  lower = true
-  upper = false
-} */
-
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+terraform {
+  
 }
 
+module  "terrahouse_aws"{
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
