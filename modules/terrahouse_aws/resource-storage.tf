a1cab86ba3430cf46ba8d88e4a0f982440e63c35
Khaled Mabrouk <mabrouk.kha@gmail.com>
2023-09-30 18:28:48 +0000

    try 4

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -24,10 +24,10 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   //source = var.index_html_filepath
-  source = "${path.root}${var.index_html_filepath}"
+  source = "${var.index_html_filepath}"
   content_type = "text/html"
   
-  etag = filemd5("${path.root}${var.index_html_filepath}")
+  etag = filemd5("${var.index_html_filepath}")
   lifecycle {
     replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [ etag ]
@@ -38,10 +38,10 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   //source = var.error_html_filepath
-  source = "${path.root}${var.error_html_filepath}"
+  source = "${var.error_html_filepath}"
   content_type = "text/html"
 
-  etag = filemd5("${path.root}${var.error_html_filepath}")
+  etag = filemd5("${var.error_html_filepath}")
   lifecycle {
     replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [ etag ]
